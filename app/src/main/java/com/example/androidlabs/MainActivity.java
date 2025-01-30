b920676b8beb5463100110b6f6dc57d5a9d3eeb8
iAmYinka <perpetualcoder@gmail.com>
2025-01-30 21:05:23 +0100

    Lab 2 Assigment

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.androidlabs;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main_linear);
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        Button button = findViewById(R.id.button);
+        EditText editText = findViewById(R.id.editText);
+        TextView textView = findViewById(R.id.textView);
+
+        button.setOnClickListener(v -> {
+            String text = editText.getText().toString();
+            textView.setText(text);
+
+            Toast.makeText(MainActivity.this,
+                    getResources().getString(R.string.toast_message),
+                    Toast.LENGTH_SHORT).show();
+        });
+
+        CheckBox checkBox = findViewById(R.id.checkbox);
+
+        checkBox.setOnCheckedChangeListener((cb, isChecked) -> {
+            Snackbar snackbar = Snackbar.make(cb,
+                    "The checkbox is now " + (isChecked ? "on" : "off"),
+                    Snackbar.LENGTH_LONG);
+
+            snackbar.setAction("Undo", v -> cb.setChecked(!isChecked));
+            snackbar.show();
         });
     }
+
 }
